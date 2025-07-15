9c8201b1e25789d6fe2b20bb30129f2a36355616
Ben John <benjohn91@hotmail.co.uk>
2025-07-15 16:56:54 +0100

    Allow users to edit their own found item reports

--- app/controllers/found_items_controller.rb
+++ app/controllers/found_items_controller.rb
@@ -1,12 +1,13 @@
 class FoundItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_found_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
     @found_items = FoundItem.all
   end
 
   def show
-    @found_item = FoundItem.find(params[:id])
   end
 
   def new
@@ -25,11 +26,9 @@ class FoundItemsController < ApplicationController
   end
 
   def edit
-    @found_item = FoundItem.find(params[:id])
   end
 
   def update
-    @found_item = FoundItem.find(params[:id])
     if @found_item.update(found_item_params)
       redirect_to @found_item, notice: "Found item updated successfully."
     else
@@ -38,17 +37,25 @@ class FoundItemsController < ApplicationController
   end
 
   def destroy
-    @found_item = FoundItem.find(params[:id])
     @found_item.destroy
     redirect_to found_items_path, notice: "Found item deleted."
   end
 
-  # ✅ New action to list current user's reports
   def my_reports
     @found_items = current_user.found_items.includes(:matches)
   end
 
   private
+
+  def set_found_item
+    @found_item = FoundItem.find(params[:id])
+  end
+
+  def authorize_user!
+    unless @found_item.user == current_user
+      redirect_to found_items_path, alert: "You are not authorized to edit this item."
+    end
+  end
 
   def found_item_params
     params.require(:found_item).permit(:title, :description, :category, :location, :date_reported, images: [])
