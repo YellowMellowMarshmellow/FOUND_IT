6f03dc41e430c885a98ae1a5b08271dc1a3c4037
Ben John <benjohn91@hotmail.co.uk>
2025-07-16 20:34:01 +0100

    Add ability for users to delete their found item reports

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_14_181134) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "claims", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "message"
+    t.string "category"
+    t.text "description"
+    t.string "title"
+    t.string "locations"
+    t.date "date_reported"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_claims_on_user_id"
+  end
+
   create_table "found_items", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -69,11 +82,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_14_181134) do
   end
 
   create_table "matches", force: :cascade do |t|
-    t.bigint "lost_items_id", null: false
+    t.bigint "claim_id", null: false
     t.bigint "found_item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["lost_items_id"], name: "index_matches_on_lost_items_id"
+    t.index ["claim_id"], name: "index_matches_on_claim_id"
     t.index ["found_item_id"], name: "index_matches_on_found_item_id"
   end
 
@@ -93,6 +106,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_14_181134) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "claims", "users"
   add_foreign_key "found_items", "users"
   add_foreign_key "lost_items", "users"
   add_foreign_key "matches", "claims"
