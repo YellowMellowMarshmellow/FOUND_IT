2e69676c6ff4376bff766b1880200a77b01afc28
Wei Hsu <weihsu89@gmail.com>
2025-07-29 14:12:53 +0200

    fixed. a user's lost report can only be match with other users' found report

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,10 +1,16 @@
 class Match < ApplicationRecord
   belongs_to :lost_item
   belongs_to :found_item
-
   attribute :confirmed, :boolean, default: false
-
   after_update :send_thank_you_if_confirmed
+
+  validate :users_cannot_be_same
+  def users_cannot_be_same
+    if lost_item && found_item && lost_item.user_id == found_item.user_id
+      errors.add(:base, "User cannot match their own lost and found items.")
+    end
+  end
+
 
   private
 
