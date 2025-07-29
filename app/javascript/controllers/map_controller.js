455bb1bbf51324c3919ce30ecaff856baa968133
Onur Soybir <o8804521@gmail.com>
2025-07-29 22:11:21 +0100

    fixed errors on match index page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,9 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      center:[this.markersValue[0].lng, this.markersValue[0].lat],
+      zoom: 12,
     })
 
     this.#addMarkersToMap()
@@ -33,7 +35,7 @@ export default class extends Controller {
   }
 
   #fitMapToMarkers() {
-  const bounds = new window.mapboxgl.LngLatBounds()
+  const bounds = new mapboxgl.LngLatBounds()
   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
 }
