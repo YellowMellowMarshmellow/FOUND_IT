c1f038641e070fcca53c6b30b8c344676a3de856
Onur Soybir <o8804521@gmail.com>
2025-07-31 19:45:53 +0100

    added map feature to show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_07_31_154624) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +87,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_31_154624) do
   create_table "notifications", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "message"
-    t.boolean "read"
+    t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "notifiable_type"
