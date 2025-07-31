2e14a1379175347662b1674a64af38cb8c1a57b3
capricia karolak <capricia@karolak.fr>
2025-07-31 18:55:26 +0200

    review notif

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_30_203910) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_31_143358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_30_203910) do
     t.datetime "updated_at", null: false
     t.string "notifiable_type"
     t.bigint "notifiable_id"
+    t.string "content"
     t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
