ad4c8744ff701d31a37ab26ccb799fc0d9f81ee2
capricia karolak <capricia@karolak.fr>
2025-08-01 23:43:02 +0200

    Create avatar for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  AVATARS = %w[bear_avatar.jpg beaver_avatar.jpg cheetah_avatar.jpg giraffe_avatar.jpg
+               hippopotamus_avatar.jpg parrot_avatar.jpg]
+  validates :avatar, inclusion: { in: AVATARS }, allow_nil: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :lost_items
